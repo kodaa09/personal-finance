44df5ef56da36cd81dc6838bb9372fca3768469e
kodaa <je.elbaze@gmail.com>
2024-09-03 23:42:14 +0200

    do transactions crud and routes

---
+++ api/app/controllers/transactions_controller.ts
@@ -0,0 +1,104 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Transaction from '#models/transaction'
+
+export default class TransactionsController {
+  /**
+   * Display a list of resource
+   */
+  async index({ response, auth }: HttpContext) {
+    const user = await auth.authenticate()
+    const transactions = await Transaction.query().where('userId', user.id).preload('category')
+
+    if (transactions) return response.status(200).json(transactions)
+    else return response.status(404).json({ error: 'Transactions not found or unauthorized' })
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, response, auth }: HttpContext) {
+    const { type, amount, description, date, recurrent } = request.only([
+      'type',
+      'amount',
+      'description',
+      'date',
+      'recurrent',
+    ])
+    const user = await auth.authenticate()
+    const transaction = await Transaction.create({
+      type,
+      amount,
+      description,
+      date,
+      recurrent,
+      userId: user.id,
+    })
+
+    return response.status(200).json(transaction)
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params, response, auth }: HttpContext) {
+    const id = params.id
+    const user = await auth.authenticate()
+
+    const transaction = await Transaction.query()
+      .where('id', id)
+      .andWhere('userId', user.id)
+      .preload('category')
+      .firstOrFail()
+
+    if (transaction) return response.status(200).json(transaction)
+    else return response.status(404).json({ error: 'Transaction not found or unauthorized' })
+  }
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request, response, auth }: HttpContext) {
+    const id = params.id
+    const user = await auth.authenticate()
+    const { type, amount, description, date, recurrent } = request.only([
+      'type',
+      'amount',
+      'description',
+      'date',
+      'recurrent',
+    ])
+    const transaction = await Transaction.findOrFail(id)
+
+    if (transaction.userId === user.id) {
+      await transaction
+        .merge({
+          type,
+          amount,
+          description,
+          date,
+          recurrent,
+          userId: user.id,
+        })
+        .save()
+      return response.status(200).json({ message: 'Transaction updated successfully' })
+    } else {
+      return response.status(404).json({ error: 'Transaction not found or unauthorized' })
+    }
+  }
+
+  /**
+   * Delete record
+   */
+  async destroy({ params, auth, response }: HttpContext) {
+    const id = params.id
+    const user = await auth.authenticate()
+    const transaction = await Transaction.findOrFail(id)
+
+    if (transaction.userId === user.id) {
+      await transaction.delete()
+      return response.status(200).json({ message: 'Transaction deleted successfully' })
+    } else {
+      return response.status(404).json({ error: 'Transaction not found or unauthorized' })
+    }
+  }
+}
--- api/start/routes.ts
+++ api/start/routes.ts
@@ -1,5 +1,8 @@
 import router from '@adonisjs/core/services/router'
 
+const TransactionsController = () => import('#controllers/transactions_controller')
+const GoalsController = () => import('#controllers/goals_controller')
+const GoalTransactionsController = () => import('#controllers/goal_transactions_controller')
 const CategoriesController = () => import('#controllers/categories_controller')
 const AuthController = () => import('#controllers/auth_controller')
 
@@ -15,5 +18,23 @@ router
     router.post('/categories', [CategoriesController, 'store'])
     router.patch('/categories/:id', [CategoriesController, 'update'])
     router.delete('/categories/:id', [CategoriesController, 'destroy'])
+
+    router.get('/transactions', [TransactionsController, 'index'])
+    router.get('/transactions/:id', [TransactionsController, 'show'])
+    router.post('/transactions', [TransactionsController, 'store'])
+    router.patch('/transactions/:id', [TransactionsController, 'update'])
+    router.delete('/transactions/:id', [TransactionsController, 'destroy'])
+
+    router.get('/goals', [GoalsController, 'index'])
+    router.get('/goals/:id', [GoalsController, 'show'])
+    router.post('/goals', [GoalsController, 'store'])
+    router.patch('/goals/:id', [GoalsController, 'update'])
+    router.delete('/goals/:id', [GoalsController, 'destroy'])
+
+    router.get('/goal-transactions', [GoalTransactionsController, 'index'])
+    router.get('/goal-transactions/:id', [GoalTransactionsController, 'show'])
+    router.post('/goal-transactions', [GoalTransactionsController, 'store'])
+    router.patch('/goal-transactions/:id', [GoalTransactionsController, 'update'])
+    router.delete('/goal-transactions/:id', [GoalTransactionsController, 'destroy'])
   })
   .prefix('api')
